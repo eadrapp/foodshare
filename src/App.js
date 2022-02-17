0778e346a372568e22e1f9e185b5b6d54048e27d
eadrapp <eadrapp@uw.edu>
2022-02-17 13:56:59 -0800

    sort of has a home page now

--- src/App.js
+++ src/App.js
@@ -3,37 +3,42 @@ import "./App.css";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import Button from "@mui/material/Button";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
 
 function App() {
   const [text, setText] = useState("");
 
   async function search() {
-    const key = "zT18GDDyLIHam78EfDyGO3yT8Q8IkZZK";
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${text}&limit=25&offset=0&lang=en`;
-    const r = await fetch(url);
-    const j = await r.json();
-    setGifs(j.data);
+    // const key = "zT18GDDyLIHam78EfDyGO3yT8Q8IkZZK";
+    // const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${text}&limit=25&offset=0&lang=en`;
+    // const r = await fetch(url);
+    // const j = await r.json();
+    // setGifs(j.data);
   }
 
   return (
-    <div className="App">
-      <div className="searchbar">
-        {/*  "outlined" variant prop gives the nice animation */}
-        <TextField
-          id="search"
-          label="Search"
-          variant="outlined"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          onKeyPress={(e) => {
-            if (e.key === "Enter") search();
-          }}
-        />
-        <Button variant="outlined" onClick={search} size="large">
-          Search Posts
-        </Button>
-      </div>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
+    // <div className="App">
+    //   <div className="searchbar">
+    //     {/*  "outlined" variant prop gives the nice animation */}
+    //     <TextField
+    //       id="search"
+    //       label="Search"
+    //       variant="outlined"
+    //       onChange={(e) => setText(e.target.value)}
+    //       value={text}
+    //       onKeyPress={(e) => {
+    //         if (e.key === "Enter") search();
+    //       }}
+    //     />
+    //     <Button variant="outlined" onClick={search} size="large">
+    //       Search Posts
+    //     </Button>
+    //   </div>
+    // </div>
   );
 }
 
